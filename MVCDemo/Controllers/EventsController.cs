2eb42d20c198fcee87c390e8b166b7d1380539f9
arusha <arusha.gudapati99@gmail.com>
2022-09-08 18:26:20 +0530

    SEPT 8

---
+++ MVCDemo/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EventsController : Controller
+    {
+        private List<Booking> events;
+
+        public IActionResult Index()
+        {
+            events = new List<Booking>();
+            events.Add(new Booking { Title = "ask CEO", EventDate = DateTime.Today, Qty = 10 });
+            return View(events);
+        }
+    }
+}
--- WebAppDETAug2022/Pages/HelloWeb.cshtml.cs
+++ WebAppDETAug2022/Pages/HelloWeb.cshtml.cs
@@ -1,17 +1,18 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using WebAppDETAug2022.Service;
 
-namespace WebAppDETAug2022.Pages
-{
-    public class HelloWebModel : PageModel
-    {
-        public string Message { get; set; }
-        public int Discount { get; set; }
-        public void OnGet()
+//namespace WebAppDETAug2022.Pages
+//{
+//    public class HelloPageModel : PageModel
+//    {
+//        public string Message { get; set; }
+//        public int Discount { get; set; }
+//        public void OnGet()
 
-        {
-            Message = "ASP.Net core is Rocking!!";
-            Discount = 15;
-        }
-    }
-}
+//        {
+//            Message = "ASP.NET Core is Rocking !";
+//            Discount = 15;
+//        }
+//    }
+//}
