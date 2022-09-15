5adc73f011b14231d7507a5b81225d7e5c2cf02d
arusha <arusha.gudapati99@gmail.com>
2022-09-15 18:01:07 +0530

    sept15

---
+++ Library.API1/Services/IBookService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Library.API1.Models;
+
+namespace Library.Api.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetByID(Guid id);
+        void Remove(Guid id);
+        object GetById(Guid id);
+    }
+}
---
+++ TDDDemo/Rooter.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TDDDemo
+{
+
+    internal class Rooter
+    {
+        
+        internal double SquareRoot(double input)
+        {
+            return Math.Sqrt(input);
+        }
+    }
+}
